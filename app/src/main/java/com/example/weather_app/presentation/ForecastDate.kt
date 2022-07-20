cec41e2a5192d97f498968501e48bad62bbf5829
g0mathew <geo.mathew@marks-and-spencer.com>
2022-07-20 11:13:18 +0530

    modify view model and mainactivity

--- app/src/main/java/com/example/weather_app/presentation/ForecastDate.kt
+++ app/src/main/java/com/example/weather_app/presentation/ForecastDate.kt
@@ -4,22 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.ImagePainter.State.Empty.painter
-import com.example.weather_app.R
 import com.example.weather_app.data.model.ResponseItem
+import com.example.weather_app.domain.di.DaggerRetroComponent
+import com.example.weather_app.domain.di.RetroComponent
 import com.example.weather_app.presentation.ui.theme.Weather_appTheme
 import com.example.weather_app.presentation.view.WeatherItem
 import com.example.weather_app.presentation.viewmodel.WeatherForecastViewModel
@@ -27,9 +22,19 @@ import com.example.weather_app.presentation.viewmodel.WeatherForecastViewModel
 class MainActivity : ComponentActivity() {
     private val weatherViewModel by viewModels<WeatherForecastViewModel>()
 
+    private lateinit var retroComponent: RetroComponent
+   // lateinit var weatherForecastViewModel:WeatherForecastViewModel
+   // @Inject
+    // lateinit var weatherForecastViewModelFactory: WeatherForecastViewModelFactory
+
     @OptIn(ExperimentalCoilApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        retroComponent = DaggerRetroComponent.builder().build()
+
+        //(application as FakerApplication).applicationComponent.inject(this)
+        //weatherForecastViewModel= ViewModelProvider(this,weatherForecastViewModelFactory).get(WeatherForecastViewModel::class.java)
+
         setContent {
             Weather_appTheme {
                 // A surface container using the 'background' color from the theme
@@ -39,6 +44,10 @@ class MainActivity : ComponentActivity() {
                 ) {
                     WeatherList(weatherList = weatherViewModel.weatherForecastResponse)
                     weatherViewModel.getWeatherForecast()
+
+                   /* weatherForecastViewModel.contentLiveData.observe(this, Observer {
+                        WeatherList(weatherList = weatherForecastViewModel.weatherForecastResponse)
+                    })*/
                 }
             }
         }
--- app/src/main/java/com/example/weather_app/presentation/viewmodel/WeatherForecastViewModel.kt
+++ app/src/main/java/com/example/weather_app/presentation/viewmodel/WeatherForecastViewModel.kt
@@ -3,14 +3,28 @@ package com.example.weather_app.presentation.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weather_app.data.model.ResponseItem
+import com.example.weather_app.data.repository.WeatherDetailsRepo
+import com.example.weather_app.domain.di.RetroServiceInterface
 import com.example.weather_app.domain.usecase.ApiData
 import kotlinx.coroutines.launch
-import java.lang.Exception
+import javax.inject.Inject
 
-class WeatherForecastViewModel : ViewModel() {
+class WeatherForecastViewModel/*(val repo: WeatherDetailsRepo)*/ : ViewModel() {
+
+/*    @Inject
+    lateinit var retroService: RetroServiceInterface
+    val contentLiveData: LiveData<List<ResponseItem>>
+    get()=repo.contents
+    init {
+        viewModelScope.launch {
+            repo.getWeatherData()
+        }
+    }*/
+
     var weatherForecastResponse: List<ResponseItem> by mutableStateOf(listOf())
 
     var errorMessage: String by mutableStateOf("")
